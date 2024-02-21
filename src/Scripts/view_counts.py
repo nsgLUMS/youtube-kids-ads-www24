aeaa4c802d1575e059c05ea0d2480463ff7df055
Aima Shahid <23100166@lums.edu.pk>
2024-02-21 15:49:10 +0500

    rename

--- src/Scripts/view_counts.py
+++ src/Scripts/view_counts.py
@@ -1,7 +1,7 @@
 from googleapiclient.discovery import build
 import csv
 
-api_key = "API_KE"
+api_key = "API_KEY"
 youtube = build("youtube", "v3", developerKey=api_key)
 
 with open('viewcount1.csv', 'w', newline='') as csvfile:
